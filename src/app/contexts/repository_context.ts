37ed365199a1f9a75e38f407657f29121d8f7122
SKAT-16 <nathangetachew536@gmail.com>
2025-12-22 10:44:31 +0300

    Add dashboard to repo

--- src/app/contexts/repository_context.ts
+++ src/app/contexts/repository_context.ts
@@ -4,6 +4,8 @@ import { HttpClient } from "@/core/api/http_client";
 import { AuthRemoteDataSource } from "@/modules/auth/infrastructure/data_sources/auth_remote_data_source";
 import { AuthRepository } from "@/modules/auth/domain/repository/auth_repository";
 import { TokenStorageService } from "@/core/services/token_storage_service";
+import { DashboardRemoteDataSource } from "@/modules/dashboard/infrastructure/data_sources/dashboard_data_remote_data_source";
+import { DashboardDataRepository } from "@/modules/dashboard/domain/repository/dashboard_data_repository";
 
 // Initialize core dependencies
 const tokenStorageService = new TokenStorageService();
@@ -17,9 +19,16 @@ const authRepository = new AuthRepository(
   authRemoteDataSource,
 );
 
+// Initialize dashboard dependencies
+const dashboardRemoteDataSource = new DashboardRemoteDataSource(httpClient);
+const dashboardRepository = new DashboardDataRepository(
+  dashboardRemoteDataSource,
+);
+
 // Initialize repositories
 export const repositories = {
   authRepository,
+  dashboardRepository, // Add the dashboard repository
 };
 
 type Repositories = typeof repositories;
