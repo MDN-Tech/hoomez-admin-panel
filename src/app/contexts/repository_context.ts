8c85b1108eae842c8e0feb676386f237f26d1085
SKAT-16 <nathangetachew536@gmail.com>
2025-12-22 15:12:55 +0300

    Add category

--- src/app/contexts/repository_context.ts
+++ src/app/contexts/repository_context.ts
@@ -6,6 +6,8 @@ import { AuthRepository } from "@/modules/auth/domain/repository/auth_repository
 import { TokenStorageService } from "@/core/services/token_storage_service";
 import { DashboardRemoteDataSource } from "@/modules/dashboard/infrastructure/data_sources/dashboard_data_remote_data_source";
 import { DashboardDataRepository } from "@/modules/dashboard/domain/repository/dashboard_data_repository";
+import { ProductCategoryRemoteDataSource } from "@/modules/product-categories/infrastructure/data_sources/category_remote_data_source";
+import { ProductCategoryRepository } from "@/modules/product-categories/domain/repository/category_repository";
 
 // Initialize core dependencies
 const tokenStorageService = new TokenStorageService();
@@ -25,10 +27,19 @@ const dashboardRepository = new DashboardDataRepository(
   dashboardRemoteDataSource,
 );
 
+// Initialize category dependencies
+const categoryRemoteDataSource = new ProductCategoryRemoteDataSource(
+  httpClient,
+);
+const categoryRepository = new ProductCategoryRepository(
+  categoryRemoteDataSource,
+);
+
 // Initialize repositories
 export const repositories = {
   authRepository,
-  dashboardRepository, // Add the dashboard repository
+  dashboardRepository,
+  categoryRepository,
 };
 
 type Repositories = typeof repositories;
