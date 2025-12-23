c7e4054084fe8b9ebba4041704b9599386eefe47
SKAT-16 <nathangetachew536@gmail.com>
2025-12-23 11:10:53 +0300

    Change create category endpoint

--- src/core/api/endpoints.ts
+++ src/core/api/endpoints.ts
@@ -10,7 +10,7 @@ export const endpoints = {
   products: {
     getCategories: "/product/categories",
     getCategoryTree: "/product/categories/tree",
-    createCategory: "/product/categories",
+    createCategory: "/product/category/create",
     updateCategory: (categoryId: string) =>
       `/product/categories/${categoryId}/update`,
 
