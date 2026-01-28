bfd2f9e82f81adb76686f1e67569d7df6e74c4ac
SKAT-16 <nathangetachew536@gmail.com>
2026-01-28 15:49:14 +0300

    Update category context to use modal-type rather than isService to handle different category types

--- src/modules/categories/presentation/contexts/CategoryProvider.tsx
+++ src/modules/categories/presentation/contexts/CategoryProvider.tsx
@@ -2,21 +2,14 @@ import type { ReactNode } from "react";
 import { CategoryContext, type CategoryContextType } from "./CategoryContext";
 
 export function CategoryProvider({
-  isService,
+  categoryType,
   children,
 }: {
-  isService: boolean;
+  categoryType: CategoryContextType;
   children: ReactNode;
 }) {
-  const value: CategoryContextType = {
-    isService,
-    moduleName: isService ? "Service Categories" : "Product Categories",
-    categoryLabel: isService ? "Service Category" : "Product Category",
-    subcategoryLabel: isService ? "Sub-service" : "Subcategory",
-  };
-
   return (
-    <CategoryContext.Provider value={value}>
+    <CategoryContext.Provider value={categoryType}>
       {children}
     </CategoryContext.Provider>
   );
